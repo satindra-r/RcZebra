d33a32d9e09db70af678fa4f5d1948ebccb5588d
Satindra Ramalingam <satindra.r@gmail.com>
2025-08-15 21:06:47 +0530

    added gnome support

--- rcZebra.sh
+++ rcZebra.sh
@@ -1,4 +1,11 @@
-for i in $(seq 1 5); do
-loginctl lock-session
-done
+if [ "$DESKTOP_SESSION" = "plasma" ]; then
+    for i in $(seq 1 5); do
+        loginctl lock-session
+    done
+else
+    while true; do
+        loginctl lock-session
+    done
+fi
+
 logout
