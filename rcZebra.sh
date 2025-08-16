9ddbeb4562eb85cfc4c76210a341cc5e758b57d5
Satindra Ramalingam <satindra.r@gmail.com>
2025-08-16 14:42:25 +0530

    hotfix cinnamon support

--- rcZebra.sh
+++ rcZebra.sh
@@ -1,11 +1,14 @@
+logout
 if [ "$DESKTOP_SESSION" = "plasma" ]; then
     for i in $(seq 1 5); do
         loginctl lock-session
     done
+elif [ "$DESKTOP_SESSION" = "cinnamon" ]; then
+    while true; do
+        cinnamon-screensaver-command --lock
+    done
 else
     while true; do
-        xdg-screensaver lock
+        gdbus call --session --dest org.gnome.ScreenSaver --object-path /org/gnome/ScreenSaver --method org.gnome.ScreenSaver.Lock
     done
 fi
-
-logout
